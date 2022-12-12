f1547e00f0fe2290f1b4524372f1f8b62396eee8
Parrr Geng <parrr.geng@gmail.com>
2022-12-12 14:10:27 +0100

    edit applicaiton and delete application routes done

--- routes/application.routes.js
+++ routes/application.routes.js
@@ -9,6 +9,36 @@ router.post("/:userId/applications/create", (req, res, next)=>{
     .catch(error => res.json(error))
 })
 
+//GET - view a specific application
+router.get("/applications/:applicationId", (req, res, next)=>{
+    const { applicationId } = req.params;
+
+    Application.findById(applicationId)
+    .then(foundApplication => res.json(foundApplication))
+    .catch(error=>res.json(error));
+})
+
+//EDIT - edit a specific application
+router.put("/applications/:applicationId/edit", (req, res, next)=>{
+    const { applicationId } = req.params;
+    
+    Application.findByIdAndUpdate(applicationId, req.body)
+    .then(foundApplication => res.json(foundApplication))
+    .catch(err => res.json(err));
+})
+
+
+//DELETE - delete a specific application 
+router.delete("/applications/:applicationId/delete", (req, res, next)=>{
+    const { applicationId } = req.params;
+
+    Application.findByIdAndRemove( applicationId )
+    .then(()=>res.json(
+        {message: "Application removed successfully."}
+    ))
+    .catch(err => res.json(err));
+});
+
 //GET - applications according to its status ["Wishlist", "Applied", "In Process", "Rejected", "Offer"]
 router.get("/:userId/applications/:status", (req, res, next)=>{
     const { userId, status } = req.params;
