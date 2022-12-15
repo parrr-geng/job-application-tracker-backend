813c3801189511281c692d8ca581f166943ee262
Parrr Geng <parrr.geng@gmail.com>
2022-12-15 16:23:16 +0100

    add credentials and delete the old possible solution

--- config/index.js
+++ config/index.js
@@ -25,14 +25,9 @@ module.exports = (app) => {
   app.use(
     cors({
       origin: [FRONTEND_URL],
+      credentials: true
     })
   );
-
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-  });
 
   // In development environment the app logs
   app.use(logger("dev"));
