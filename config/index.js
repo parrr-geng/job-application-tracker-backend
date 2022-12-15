0f29aeb1fbf3469227cbd74946c81ae8969aad41
Parrr Geng <parrr.geng@gmail.com>
2022-12-15 16:42:27 +0100

    testing cors setting change

--- config/index.js
+++ config/index.js
@@ -24,7 +24,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: "https://apricot-clam-robe.cyclic.app",
+      origin: FRONTEND_URL,
       credentials: true
     })
   );
